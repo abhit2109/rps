ba48319fefb835421ed12479d4f79906d3ea4c50
abhit2109 <122540059+abhit2109@users.noreply.github.com>
2023-01-31 10:11:24 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,4 @@
-input.onGesture(Gesture.Shake, function () {
-    reset()
-})
-function reset () {
-    OLED.init(128, 64)
-    Rounds = 0
-    Ties = 0
-    P1 = 0
-    P2 = 0
-    OLED.writeStringNewLine("Shall We Play A Game?")
-    basic.pause(2000)
+function updateScoreboard () {
     OLED.clear()
     OLED.writeStringNewLine("Player 1 Score: " + P1)
     OLED.newLine()
@@ -18,11 +8,64 @@ function reset () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds:" + Rounds)
 }
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    P1 += 1
+    Rounds += 1
+    updateScoreboard()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    Ties += 1
+    Rounds += 1
+    updateScoreboard()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    P2 += 1
+    Rounds += 1
+    updateScoreboard()
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    Rounds = 0
+    Ties = 0
+    Ties += 1
+    P1 = 0
+    P2 = 0
+    OLED.writeStringNewLine("Shall We Play A Game?")
+    basic.pause(1000)
+    updateScoreboard()
+}
+let Rounds = 0
+let Ties = 0
 let P2 = 0
 let P1 = 0
-let Ties = 0
-let Rounds = 0
 reset()
-basic.forever(function () {
-	
-})
